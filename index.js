fd4fc8ddfefaaf0888067aff8dacd3742a039deb
Brady <vanderbannond@gmail.com>
2018-03-13 20:48:02 -0700

    axios post working, show and hide blog/form content on right side working

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ document.addEventListener('DOMContentLoaded', ev => {
   // callOtherDomain()
 });
 
-function renderBlogPosts(blogPosts) {
-  document.getElementById('blog-title').innerHTML = blogPosts.data[0].title
-  document.getElementById('blog-content').innerHTML = blogPosts.data[0].content
-  document.getElementById('blog-image').innerHTML = blogPosts.data[0].image
-
-}
+// function renderBlogPosts(blogPosts) {
+//   document.getElementById('blog-title').innerHTML = blogPosts.data[0].title
+//   document.getElementById('blog-content').innerHTML = blogPosts.data[0].content
+//   document.getElementById('blog-image').innerHTML = blogPosts.data[0].image
+//
+// }
 
 function getBlogPosts () {
   let blogPosts
@@ -36,29 +36,71 @@ function getBlogPosts () {
     document.getElementById('blog-title').innerHTML = blogPosts.data[0].title
     document.getElementById('blog-content').innerHTML = blogPosts.data[0].content
     document.getElementById('blog-image').setAttribute('src', blogPosts.data[0].image)
+    blogPosts.data.forEach(index => {
+      let button = document.createElement('button')
+      button.addEventListener('click', ev => {
+        console.log('ello');
+        let contentWindow = document.getElementById('show-post')
+        let formArray = document.querySelectorAll('.post-form')
+        formArray.forEach(index => {
+         index.style.display = 'none'
+        })
+        contentWindow.style.display = 'block'
+      })
+      let insertionPoint = document.getElementById('post-selection')
+      insertionPoint.innerHTML = ""
+      button.textContent = index.title
+      insertionPoint.append(button)
+    })
+
     console.log(blogPosts);
   })
   .catch(err => {
     console.log(err);
   })
-  console.log(blogPosts[0].title);
-  renderBlogPosts(blogPosts)
+  // console.log(blogPosts[0].title);
+  // renderBlogPosts(blogPosts)
 }
 
 function init(ev) {
   console.log('** INITIALIZE **');
   document.getElementById('new').addEventListener('click', showPostForm)
+  $( "#save" ).click(function( event ) {
+  createPost()
+});
+  $('form').submit(event => {
+    event.preventDefault();
+  });
   getBlogPosts()
 }
 
 // display the form
 let showPostForm = () => {
-  // show/hide the form
+  console.log('clicked show form');
+  let formArray = document.querySelectorAll('.post-form')
+  let visibleContent = document.getElementById('show-post')
+  visibleContent.style.display = 'none'
+  formArray.forEach(index => {
+   index.style.display = 'block'
+  })
 }
 
 // when the form is submitted
 let createPost = () => {
-    // get values from form
-    // axios.post()
-    // after the post succeeds, show a message
+    let title = document.getElementById('title').value
+    let content = document.getElementById('content').value
+    let image = document.getElementById('image').value
+    let displayMessage = document.getElementById("save-status")
+    axios.post('http://localhost:3000/posts', { title, content, image })
+    .then(response => {
+     console.log(response);
+     displayMessage.innerHTML = "Success!"
+     $('#save-status').fadeIn(500).delay(2000).fadeOut(500)
+   })
+   .catch(error => {
+     console.log(error);
+     displayMessage.innerHTML = "Not quite"
+     $('#save-status').fadeIn(500).delay(2000).fadeOut(500)
+   })
+    getBlogPosts()
 }
